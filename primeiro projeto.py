714fc07852a774086a50b54448e79061490b16d4
luisreginaldolima2-commits <luisreginaldolima2@gmail.com>
2026-03-23 07:53:21 -0300

    Add files via upload

---
+++ primeiro projeto.py
@@ -0,0 +1,6 @@
+#Programa de Notas
+NOME = imput("digite o nome do aluno")
+NOTA1 = imput("digite a primeira nota")
+NOTA2 = imput("digite a seguda nota")
+MEDIA = NOTA1+NOTA2/2
+print("O aluno ",NOME, "tem media",MEDIA)
